7e16096e1612aba8f3ba8e3656021e49a8d46bc0
Dinh Truong <43572251+DinhTruong98@users.noreply.github.com>
2019-12-19 19:40:57 +0700

    create new class for 3-layer template

---
+++ DTO/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+        public Boolean Sex { get; set; }
+    }
+}
